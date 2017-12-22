97745300a6e067d7330e7558250a938e0ecb3b3d
LauraJH16 <laurajimenezh16@hotmail.com>
2017-12-22 12:09:07 -0500

    Input Terminados

---
+++ js/app.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+    var username = $('#username');
+    var password = $('#password');
+    var confirmPassword = $('#confirm_password');
+    var submit = $('#submit');
+    
+    $(username).keyup(function(){
+    if($(username).val().length > 3){
+        $('.span-user').hide();
+    }else{
+        $('.span-user').show();
+    }
+    });
+
+    $(password).keyup(function(){
+        if($(password).val().length > 6){
+            $('.span-usertwo').hide();
+        }else{
+            $('.span-usertwo').show();
+        }
+        });   
+
+        $(confirmPassword).keyup(function(){
+            if($(confirmPassword).val() === $(password).val()){
+                $('.span-userthree').hide();
+            }else{
+                $('.span-userthree').show();
+            }
+            });
+
+            $(password).keyup(function(){
+
+                if($(confirmPassword).val() != ''){
+                    if($(confirmPassword).val() === $(password).val()){
+                        $('.span-userthree').hide();
+                    }else{
+                        $('.span-userthree').show();
+                    }
+                }
+                });
+
+
+        
+
+
+});
