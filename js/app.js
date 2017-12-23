6add36d00098c7f954c6f19459dfae1fbf324b5f
LauraJH16 <laurajimenezh16@hotmail.com>
2017-12-22 22:49:59 -0500

    Proyecto Terminado

--- js/app.js
+++ js/app.js
@@ -1,47 +1,44 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  var username = $('#username');
+  var password = $('#password');
+  var confirmPassword = $('#confirm_password');
+  var submit = $('#submit');
 
-    var username = $('#username');
-    var password = $('#password');
-    var confirmPassword = $('#confirm_password');
-    var submit = $('#submit');
-    
-    $(username).keyup(function(){
-    if($(username).val().length > 3){
-        $('.span-user').hide();
-    }else{
-        $('.span-user').show();
+  $(username).keyup(function () {
+    if ($(username).val().length > 3) {
+      $('.span-user').hide();
+    } else {
+      $('.span-user').show();
     }
-    });
+  });
 
-    $(password).keyup(function(){
-        if($(password).val().length > 6){
-            $('.span-usertwo').hide();
-        }else{
-            $('.span-usertwo').show();
-        }
-        });   
+  $(password).keyup(function () {
+    if ($(password).val().length > 6) {
+      $('.span-usertwo').hide();
+    } else {
+      $('.span-usertwo').show();
+    }
 
-        $(confirmPassword).keyup(function(){
-            if($(confirmPassword).val() === $(password).val()){
-                $('.span-userthree').hide();
-            }else{
-                $('.span-userthree').show();
-            }
-            });
+    if ($(confirmPassword).val() != '') {
+      if ($(confirmPassword).val() === $(password).val()) {
+        $('.span-userthree').hide();
+      } else {
+        $('.span-userthree').show();
+      }
+    }
 
-            $(password).keyup(function(){
+  });
 
-                if($(confirmPassword).val() != ''){
-                    if($(confirmPassword).val() === $(password).val()){
-                        $('.span-userthree').hide();
-                    }else{
-                        $('.span-userthree').show();
-                    }
-                }
-                });
+  $(confirmPassword).keyup(function () {
+    if ($(confirmPassword).val() === $(password).val()) {
+      $('.span-userthree').hide();
+    } else {
+      $('.span-userthree').show();
+    }
 
-
-        
-
+    if ($(username).val().length > 3 && $(password).val().length > 6 && $(confirmPassword).val() === $(password).val()) {
+      $(submit).attr('disabled', false);
+    }
+  });
 
 });
