099544e00b8e6478326d2ee2373c33606d76b083
LauraJH16 <laurajimenezh16@hotmail.com>
2017-12-22 22:55:22 -0500

    Proyecto Terminado

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
+$(document).ready(function() {
   var username = $('#username');
   var password = $('#password');
   var confirmPassword = $('#confirm_password');
   var submit = $('#submit');
 
-  $(username).keyup(function () {
+  $(username).keyup(function() {
     if ($(username).val().length > 3) {
       $('.span-user').hide();
     } else {
@@ -12,24 +12,23 @@ $(document).ready(function () {
     }
   });
 
-  $(password).keyup(function () {
+  $(password).keyup(function() {
     if ($(password).val().length > 6) {
       $('.span-usertwo').hide();
     } else {
       $('.span-usertwo').show();
     }
 
-    if ($(confirmPassword).val() != '') {
+    if ($(confirmPassword).val() !== '') {
       if ($(confirmPassword).val() === $(password).val()) {
         $('.span-userthree').hide();
       } else {
         $('.span-userthree').show();
       }
     }
-
   });
 
-  $(confirmPassword).keyup(function () {
+  $(confirmPassword).keyup(function() {
     if ($(confirmPassword).val() === $(password).val()) {
       $('.span-userthree').hide();
     } else {
@@ -40,5 +39,4 @@ $(document).ready(function () {
       $(submit).attr('disabled', false);
     }
   });
-
 });
